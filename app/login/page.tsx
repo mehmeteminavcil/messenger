e398e61d2485470177f2d39a61b5a32f29c5aeba
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2024-06-17 20:33:46 +0300

    Remove .env file

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,8 @@
+import { Metadata } from "next";
 import Signup from "../ui/Signup";
-
+export const metadata: Metadata = {
+  title: "Login",
+};
 const page = () => {
   return (
     <section className="w-full p-3 items-center justify-center rounded-3xl bg-white border border-gray max-w-[1240px] flex h-full overflow-hidden">
